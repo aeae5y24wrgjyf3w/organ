fd9022370e50bf781a34615b53e756d04b765e6b
aeae5y24wrgjyf3w <ttttotsuka@gmail.com>
2026-05-25 23:59:20 +0900

    Update vowels.c
    誤ったパラメータを修正。さらに男声と女声に分けた。

--- sin/instruments/vowels.c
+++ sin/instruments/vowels.c
@@ -10,19 +10,34 @@ AMP formant(AMP f, AMP c, AMP w, AMP g)
 
 #define HZ(x) (x / 24000.0)
 
-AMP vowel_a(FREQ f, FREQ f1, AMP aa)
+AMP vowel_female_a(FREQ f, FREQ f1, AMP aa)
 {
 	AMP r = (AMP)f1 / (AMP)f;
-	return aa * r * r *
+	return aa * r * r * 0.8 *
 		(
 		 1
-		 *formant((AMP)f / (AMP)FREQ_MID, HZ(1200.0), HZ(2000.0) , 1.0)
-		 //+formant((AMP)f / (AMP)FREQ_MID, HZ(1200.0), HZ(200.0), 0.5)
-		 //+formant((AMP)f / (AMP)FREQ_MID, HZ(2500.0), HZ(300.0), 0.25)
+		 *formant((AMP)f / (AMP)FREQ_MID, HZ(1000.0), HZ(150.0) , 1.0)
+		 +formant((AMP)f / (AMP)FREQ_MID, HZ(1500.0), HZ(250.0), 0.7)
+		 +formant((AMP)f / (AMP)FREQ_MID, HZ(3120.0), HZ(375.0), 0.4)
+		);
+}
+
+AMP vowel_male_a(FREQ f, FREQ f1, AMP aa)
+{
+	AMP r = (AMP)f1 / (AMP)f;
+	return aa * r * r * 
+		(
+		 1
+		 *formant((AMP)f / (AMP)FREQ_MID, HZ(800.0), HZ(120.0) , 1.0)
+		 +formant((AMP)f / (AMP)FREQ_MID, HZ(1200.0), HZ(200.0), 0.7)
+		 +formant((AMP)f / (AMP)FREQ_MID, HZ(2500.0), HZ(300.0), 0.4)
 		);
 }
 
 INSTRUMENT select_instrument(int note, int velocity, int channel)
 {
-	return vowel_a;
+	if (channel & 1)
+		return vowel_male_a;
+	else
+		return vowel_female_a;
 }
